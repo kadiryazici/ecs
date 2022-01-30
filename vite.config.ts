4a782278cef31c16c05647bed15523b55795a304
Kadir Yazıcı <kyzc411@outlook.com.tr>
2022-01-30 23:35:33 +0300

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ const productionConfig = defineConfig({
 
 const demoConfig = defineConfig({
    root: path.join(root, 'demo'),
-   base: '/base/',
+   base: '/ecs/',
    build: {
       emptyOutDir: true,
       outDir: path.join(root, 'dist-demo'),
