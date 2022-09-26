d09507f7c18d832f4936b8b36d78809654ea7d02
kadiryazici <kyzc411@outlook.com.tr>
2022-09-27 02:54:10 +0300

    Add type prefix

--- src/lib.ts
+++ src/lib.ts
@@ -2,5 +2,5 @@ export { Entity, EntityId, createEntity } from './Entity';
 export { Query, createQuery } from './Query';
 export { With, Without } from './Query/helpers';
 export type { QueryModifier, MapQueryReturn, QueryComponents } from './Query/types';
-export { Component, ComponentDescriptor, type DefaultStateType, defineComponent } from './Component';
+export { Component, type ComponentDescriptor, type DefaultStateType, defineComponent } from './Component';
 export { World, createWorld } from './World';
